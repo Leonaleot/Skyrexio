82f6dc7cb03a9d7e836d8a4c3046dbea541cc413
Tatiana <leonaleot@mail.ru>
2026-04-04 18:29:33 +0300

    BaseTest is added

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized"); //Раскрыть браузер на весь экран
+        options.addArguments("headless");        // Браузер запускается без графического интерфейса
+        options.addArguments("guest");           // Гостевой режим
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //Ожидание 10с, если элемент не найден
+    }
+
+    @AfterMethod
+    public void close() {
+        driver.quit();
+    }
+}
